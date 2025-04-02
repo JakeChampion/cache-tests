0111d25325554783f41e25b7ac082fe1a432e6d9
jake champion <jake.champion@netlify.com>
2025-04-02 10:46:09 +0100

    meow

--- netlify/functions/proxy.mjs
+++ netlify/functions/proxy.mjs
@@ -7,9 +7,12 @@ export default async (req) => {
   url.host = 'http://c8koc4ks0wkokgckoo8gck0o.164.90.159.101.sslip.io'
   url.port = 80
   url.protocol = 'http'
+  console.log(url.toString())
   const rreeqq = new Request(url, req)
   rreeqq.headers.set('host', url.host)
+  console.log({rreeqq})
   const response = await fetch(rreeqq)
+  console.log({response})
   return response
 }
 
