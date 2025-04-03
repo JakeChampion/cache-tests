f7262696ac7f79488debb0e081e2e59730105759
jake champion <jake.champion@netlify.com>
2025-04-03 12:12:11 +0100

    nice

--- results/index.mjs
+++ results/index.mjs
@@ -1,23 +1,4 @@
 export default [
-  // {
-  //   file: 'chrome.json',
-  //   name: 'Chrome',
-  //   type: 'browser',
-  //   version: '132.0.6834.84'
-  // },
-  // {
-  //   file: 'firefox.json',
-  //   name: 'Firefox',
-  //   type: 'browser',
-  //   version: '134.0.1',
-  //   link: 'https://github.com/http-tests/cache-tests/wiki/Firefox'
-  // },
-  // {
-  //   file: 'safari.json',
-  //   name: 'Safari',
-  //   type: 'browser',
-  //   version: '18.2 (20620.1.16.11.8)'
-  // },
   {
     file: 'nginx.json',
     name: 'nginx',
@@ -67,13 +48,13 @@ export default [
     version: '3.0.8-1ubuntu1',
     link: 'https://github.com/http-tests/cache-tests/wiki/HAProxy'
   },
-  // {
-  //   file: 'fastly.json',
-  //   name: 'Fastly',
-  //   type: 'cdn',
-  //   version: '2025-01-21',
-  //   link: 'https://github.com/http-tests/cache-tests/wiki/Fastly'
-  // },
+  {
+    file: 'fastly.json',
+    name: 'Fastly',
+    type: 'cdn',
+    version: '2025-01-21',
+    link: 'https://github.com/http-tests/cache-tests/wiki/Fastly'
+  },
   {
     file: 'netlify.json',
     name: 'Netlify',
