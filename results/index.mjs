805f3dc6f08e20d5a826f09ca14fbbde034ba34e
jake champion <jake.champion@netlify.com>
2025-04-03 14:51:37 +0100

    lol

--- results/index.mjs
+++ results/index.mjs
@@ -1,18 +1,18 @@
 export default [
-  {
-    file: 'nginx.json',
-    name: 'nginx',
-    type: 'rev-proxy',
-    version: '1.26.0-3ubuntu1',
-    link: 'https://github.com/http-tests/cache-tests/wiki/nginx'
-  },
-  {
-    file: 'squid.json',
-    name: 'Squid',
-    type: 'rev-proxy',
-    version: '6.10-1ubuntu1',
-    link: 'https://github.com/http-tests/cache-tests/wiki/Squid'
-  },
+  // {
+  //   file: 'nginx.json',
+  //   name: 'nginx',
+  //   type: 'rev-proxy',
+  //   version: '1.26.0-3ubuntu1',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/nginx'
+  // },
+  // {
+  //   file: 'squid.json',
+  //   name: 'Squid',
+  //   type: 'rev-proxy',
+  //   version: '6.10-1ubuntu1',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/Squid'
+  // },
   {
     file: 'trafficserver.json',
     name: 'ATS',
@@ -20,20 +20,20 @@ export default [
     version: '9.2.5+ds-1',
     link: 'https://github.com/http-tests/cache-tests/wiki/Traffic-Server'
   },
-  {
-    file: 'apache.json',
-    name: 'httpd',
-    type: 'rev-proxy',
-    version: '2.4.62-3ubuntu1',
-    link: 'https://github.com/http-tests/cache-tests/wiki/Apache-httpd'
-  },
-  {
-    file: 'varnish.json',
-    name: 'Varnish',
-    type: 'rev-proxy',
-    version: '7.5.0-3',
-    link: 'https://github.com/http-tests/cache-tests/wiki/Varnish'
-  },
+  // {
+  //   file: 'apache.json',
+  //   name: 'httpd',
+  //   type: 'rev-proxy',
+  //   version: '2.4.62-3ubuntu1',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/Apache-httpd'
+  // },
+  // {
+  //   file: 'varnish.json',
+  //   name: 'Varnish',
+  //   type: 'rev-proxy',
+  //   version: '7.5.0-3',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/Varnish'
+  // },
   {
     file: 'caddy.json',
     name: 'caddy',
@@ -41,13 +41,13 @@ export default [
     version: '0.7.0',
     link: 'https://github.com/http-tests/cache-tests/wiki/Caddy'
   },
-  {
-    file: 'haproxy.json',
-    name: 'HAProxy',
-    type: 'rev-proxy',
-    version: '3.0.8-1ubuntu1',
-    link: 'https://github.com/http-tests/cache-tests/wiki/HAProxy'
-  },
+  // {
+  //   file: 'haproxy.json',
+  //   name: 'HAProxy',
+  //   type: 'rev-proxy',
+  //   version: '3.0.8-1ubuntu1',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/HAProxy'
+  // },
   {
     file: 'fastly.json',
     name: 'Fastly',
@@ -63,7 +63,7 @@ export default [
   },
   {
     file: 'vercel.json',
-    name: 'Cercel',
+    name: 'Vercel',
     type: 'cdn',
     version: '2025-04-03'
   },
