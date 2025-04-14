4c615f3f642e4bf4fada0e5ac346988a03888c67
jake champion <jake.champion@netlify.com>
2025-04-14 14:44:56 +0100

    ok

--- results/index.mjs
+++ results/index.mjs
@@ -12,14 +12,14 @@ export default [
   //   type: 'rev-proxy',
   //   version: '6.10-1ubuntu1',
   //   link: 'https://github.com/http-tests/cache-tests/wiki/Squid'
+  // // },
+  // {
+  //   file: 'trafficserver.json',
+  //   name: 'ATS',
+  //   type: 'rev-proxy',
+  //   version: '9.2.5+ds-1',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/Traffic-Server'
   // },
-  {
-    file: 'trafficserver.json',
-    name: 'ATS',
-    type: 'rev-proxy',
-    version: '9.2.5+ds-1',
-    link: 'https://github.com/http-tests/cache-tests/wiki/Traffic-Server'
-  },
   // {
   //   file: 'apache.json',
   //   name: 'httpd',
@@ -34,43 +34,43 @@ export default [
   //   version: '7.5.0-3',
   //   link: 'https://github.com/http-tests/cache-tests/wiki/Varnish'
   // },
-  {
-    file: 'caddy.json',
-    name: 'caddy',
-    type: 'rev-proxy',
-    version: '0.7.0',
-    link: 'https://github.com/http-tests/cache-tests/wiki/Caddy'
-  },
+  // {
+  //   file: 'caddy.json',
+  //   name: 'caddy',
+  //   type: 'rev-proxy',
+  //   version: '0.7.0',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/Caddy'
+  // },
   // {
   //   file: 'haproxy.json',
   //   name: 'HAProxy',
   //   type: 'rev-proxy',
   //   version: '3.0.8-1ubuntu1',
   //   link: 'https://github.com/http-tests/cache-tests/wiki/HAProxy'
+  // // },
+  // {
+  //   file: 'fastly.json',
+  //   name: 'Fastly',
+  //   type: 'cdn',
+  //   version: '2025-01-21',
+  //   link: 'https://github.com/http-tests/cache-tests/wiki/Fastly'
   // },
-  {
-    file: 'fastly.json',
-    name: 'Fastly',
-    type: 'cdn',
-    version: '2025-01-21',
-    link: 'https://github.com/http-tests/cache-tests/wiki/Fastly'
-  },
   {
     file: 'netlify.json',
     name: 'Netlify',
     type: 'cdn',
     version: '2025-04-03'
   },
-  {
-    file: 'vercel.json',
-    name: 'Vercel',
-    type: 'cdn',
-    version: '2025-04-03'
-  },
-  {
-    file: 'cloudflare.json',
-    name: 'Cloudflare',
-    type: 'cdn',
-    version: '2025-04-03'
-  },
+  // {
+  //   file: 'vercel.json',
+  //   name: 'Vercel',
+  //   type: 'cdn',
+  //   version: '2025-04-03'
+  // },
+  // {
+  //   file: 'cloudflare.json',
+  //   name: 'Cloudflare',
+  //   type: 'cdn',
+  //   version: '2025-04-03'
+  // },
 ]
